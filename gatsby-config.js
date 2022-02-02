0ae00dcedf072fbd31b1b3a596fd035dd647065d
megob56 <obrien.e.meaghan@gmail.com>
2022-02-02 17:30:12 +0000

    chore:initial setup of gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,25 @@ module.exports = {
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `pages`,
+                path: `${__dirname}/src/pages/`,
+            },
+        },
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                tableOfContents: {
+                    heading: null,
+                    maxDepth: 6,
+                },
+            },
+        },
+        `gatsby-plugin-mdx`,
+        `gatsby-plugin-image`,
+        `gatsby-plugin-sharp`,
 
     ]
 }
